ddb7b4455eb69ed29ddad3cb5e42f256f1beb8cb
Subodh-112 <subodhchauhan8880@gmail.com>
2023-09-19 17:15:43 +0530

    image

--- src/types/flow/webhook.d.ts
+++ src/types/flow/webhook.d.ts
@@ -1,3 +1,90 @@
+// import { ObjectId } from "mongodb";
+
+// export interface iWebhookPayload {
+//   object: string;
+//   entry: {
+//     id: string;
+//     changes: {
+//       value: {
+//         messaging_product: "whatsapp";
+//         metadata: {
+//           display_phone_number: string;
+//           phone_number_id: string;
+//         };
+//         contacts: {
+//           wa_id: string;
+//           profile: {
+//             name: string;
+//           };
+//         }[];
+//         errors: {
+//           code: number;
+//           title: string;
+//         }[];
+//         messages: {
+//           button?: iButtonMessagePayload;
+//           interactive?: iReplyMessagePayload | iListMessagePayload;
+//           text?: iTextMessagePayload;
+//           image?: iImageMessage;
+//         }[];
+//         statuses: [];
+//       };
+//       field: "messages";
+//     }[];
+//   }[];
+// }
+
+// interface iButtonMessagePayload {
+//   payload: any;
+//   text: string;
+// }
+
+// interface iReplyMessagePayload {
+//   type: "button_reply";
+//   button_reply: {
+//     id: string;
+//     title: string;
+//   };
+// }
+
+// interface iListMessagePayload {
+//   type: "list_reply";
+//   list_reply: {
+//     id: string;
+//     title: string;
+//     description: string;
+//   };
+// }
+
+// interface iTextMessagePayload {
+//   body: string;
+// }
+
+// interface iTextMessage {
+//   text: string;
+//   sender: string;
+//   type: "received" | "sent";
+//   ticket: string;
+//   consumer: string;
+//   messageType: "text";
+//   createdAt: number;
+// }
+
+// interface iImageMessage {
+//   id:string;
+//   imageUrl: string;
+//   caption?: string;
+//   sender: string;
+//   type: "received";
+//   ticket: string;
+//   consumer: string;
+// messageType:"image";
+//   createdAt: number;
+// }
+
+
+
+
 import { ObjectId } from "mongodb";
 
 export interface iWebhookPayload {
@@ -25,7 +112,7 @@ export interface iWebhookPayload {
           button?: iButtonMessagePayload;
           interactive?: iReplyMessagePayload | iListMessagePayload;
           text?: iTextMessagePayload;
-          image?: iImageMessage;
+          image?: iImageMessagePayload;
         }[];
         statuses: [];
       };
@@ -59,6 +146,10 @@ interface iListMessagePayload {
 interface iTextMessagePayload {
   body: string;
 }
+interface iImageMessagePayload {
+  url: any; // URL or identifier for the image
+  caption?: string; // Optional caption for the image
+}
 
 interface iTextMessage {
   text: string;
@@ -71,13 +162,12 @@ interface iTextMessage {
 }
 
 interface iImageMessage {
-  id:string;
-  imageUrl: string;
-  caption?: string;
-  sender: string;
-  type: "received";
-  ticket: string;
   consumer: string;
-messageType:"image";
+  sender: string;
+  imageUrl: string;
+  caption: string;
+  ticket: string;
+  type: "received";
+  messageType: "image";
   createdAt: number;
 }
